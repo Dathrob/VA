8efc4f2ae197d515dea0665be462603204d252fd
dathrob <dev.misganaw.meseret@gmail.com>
2021-08-20 21:41:02 +0300

    listens to amharic and opens on yt

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import speech_recognition as sr
 import pyttsx3
 from googletrans import Translator, constants
+import pywhatkit
 translator = Translator()
 
 listner = sr.Recognizer()
-engine = pyttsx3.init()  
+engine = pyttsx3.init()
+engine.setProperty('rate', 100)
 
 try:
     with sr.Microphone() as source:
+       
+       
         print("listning")
         voice = listner.listen(source)
         command = listner.recognize_google(voice,language="am-ET")
         translation = translator.translate(command)
         t_command = translation.text
-        engine.say(t_command)
-        engine.runAndWait()
+        #engine.say(t_command)
+        #engine.runAndWait()
         print(command)
+        pywhatkit.playonyt(t_command)
+        
 except:
     print("mic is not working")
     
