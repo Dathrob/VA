fb1842e8170fe5877f9f2260de88ca3b75649aaf
dathrob <dev.misganaw.meseret@gmail.com>
2021-08-20 18:52:33 +0300

    recongnize amharic and translate to english

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
 from googletrans import Translator, constants
+translator = Translator()
 
 listner = sr.Recognizer()
 engine = pyttsx3.init()  
@@ -10,8 +11,9 @@ try:
         print("listning")
         voice = listner.listen(source)
         command = listner.recognize_google(voice,language="am-ET")
-
-        engine.say(command)
+        translation = translator.translate(command)
+        t_command = translation.text
+        engine.say(t_command)
         engine.runAndWait()
         print(command)
 except:
