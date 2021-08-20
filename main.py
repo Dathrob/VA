1c071ef269d681e8277629119d020a4eb38f9b9b
dathrob <dev.misganaw.meseret@gmail.com>
2021-08-20 18:44:29 +0300

    working listner engine

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import speech_recognition as sr
+import pyttsx3
+from googletrans import Translator, constants
 
 listner = sr.Recognizer()
+engine = pyttsx3.init()  
 
 try:
     with sr.Microphone() as source:
+        print("listning")
         voice = listner.listen(source)
+        command = listner.recognize_google(voice,language="am-ET")
+
+        engine.say(command)
+        engine.runAndWait()
+        print(command)
 except:
-    pass
+    print("mic is not working")
+    
