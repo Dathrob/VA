9f295151728a88b4af3604fb98d493022f20826a
dathrob <dev.misganaw.meseret@gmail.com>
2021-08-21 18:39:25 +0300

    added functions and clean up

--- main.py
+++ main.py
@@ -7,53 +7,57 @@ from playsound import playsound
 translator = Translator()
 listner = sr.Recognizer()
 engine = pyttsx3.init()
-engine.setProperty('rate', 120)
-#engine.say("Hello  misganaw  how  can  i  help  you")
-#engine.runAndWait()
 playsound('synthesize.mp3')
-def search(query):
+def searcher(query):
+    print("search is called")
     pywhatkit.search(query)
     starter()
+
+
+
 def open(application):
+    print("open is called")
     engine.say("opening"+application)
     engine.runAndWait()
     os.system(application)
     starter()
-    
+
+
+
+def youtube(item):
+    pywhatkit.playonyt(item)
+
+
+
 def starter():
     try:
         with sr.Microphone() as source:
-           
             print("listning")
             voice = listner.listen(source,phrase_time_limit=4)
             command = listner.recognize_google(voice,language="am-ET")
             print(command)
-
-            if("ክፈት" or "ክፍት" or "ከፈተልኝ" or"ከፍ ሲል" in command):
+            if(("ቪዲዮ" in command)or("video" in command)):
+                print("video found")
+                query = command.replace("ቪዲዮ","")
+                youtube(command)
+            elif(("ፍለጋ" in command) or ("በመፈለግ ላይ" in command) or ("ፈልግ" in command)):
+                query = command.replace("ፈልግ"," ")
+                searcher(command)
+            elif(("ክፈት" in command) or ("ክፍት" in command) or ("ከፈተልኝ" in command) or("ከፍ ሲል" in command)):
                 application = command.replace("ክፈት","")
                 application = command.replace("ክፍት","")
                 application = command.replace("ከፈተልኝ","")
                 application = command.replace("ከፍ ሲል","")
                 translation = translator.translate(application)
                 open_command = translation.text
-                print("translated"+open_command)
                 open(open_command)
-            elif("ፍለጋ" or"በመፈለግ ላይ" or "ፈልግ" in command ):
-                query = command.replace("ፈልግ"," ")
-                translation = translator.translate(query)
-                t_command = translation.text
-                search(t_command)
-            elif("ቪዲዮ" in command):
-                translation = translator.translate(command)
-                t_command = translation.text
-                query = command.replace("ቪዲዮ","")
-                
             else:
-                engine.say("sorry the command is unkown")
+                playsound('synthesize (1).mp3')
                 starter()
-            #pywhatkit.playonyt(t_command)
+            
         
     except:
-        print("mic is not working")
+        engine.say("sorry command not found")
 
 starter()
+
